d76ccfd0552d36b2995f46b1e1ade9da1998035e
Dhvanik Patel <pateldhvanik789@gmail.com>
2024-03-14 09:46:33 +0530

    ChatBot endpoint added using openapi

---
+++ router/chatBot.js
@@ -0,0 +1,37 @@
+import OpenAI from "openai";
+import dotenv from 'dotenv';
+import { Router } from "express";
+
+dotenv.config();
+
+const router = new Router();
+
+const openai = new OpenAI({
+   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+
+router.post('/api/chat', async (req, res) => {
+   const { prompt } = req.body;
+   try {
+      const response = await openai.chat.completions.create({
+         model: "gpt-3.5-turbo",
+         messages: [
+            {
+               role: "user",
+               content: prompt
+            }
+         ],
+         temperature: 1,
+         max_tokens: 256,
+         top_p: 1,
+         frequency_penalty: 0,
+         presence_penalty: 0,
+      });
+
+      return res.send(response.choices[0].message.content);
+   } catch {
+      return res.status(500).send({ status: 'error', message: 'Error requesting AI bot.' });
+   }
+})
+
+export default router;
--- router/manageRoutes.js
+++ router/manageRoutes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import SignupRouter from './signup.js';
 import LoginRouter from './login.js';
 import UserRoter from './user.js';
+import ChatBotRouter from './chatBot.js';
 const router = new Router();
 
 router.use(SignupRouter);
 router.use(LoginRouter);
 router.use(UserRoter);
+router.use(ChatBotRouter);
 
 export default router;
